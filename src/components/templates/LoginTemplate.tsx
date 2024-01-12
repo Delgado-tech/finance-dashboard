a67c175d06fa50ec6fe4e26327bf9ac573ff259b
Delgado-tech <leonardo.delgadosp2014@gmail.com>
2024-01-12 09:16:06 -0300

    start structure

---
+++ src/components/templates/LoginTemplate.tsx
@@ -0,0 +1,7 @@
+export default function LoginTemplate() {
+	return (
+		<section>
+			<main>login</main>
+		</section>
+	);
+}
---
+++ src/components/templates/RegisterTemplate.tsx
@@ -0,0 +1,7 @@
+export default function RegisterTemplate() {
+	return (
+		<section>
+			<main>register</main>
+		</section>
+	);
+}
